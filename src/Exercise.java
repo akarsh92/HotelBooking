a6dc7893aa4b43326d3c8b5535bdbbb1d2b05135
akarsh <akarsh92@gmail.com>
2018-05-12 20:33:36 -0500

    New Changes

--- src/Exercise.java
+++ src/Exercise.java
@@ -31,8 +31,14 @@ public class Exercise {
 
 			ArrayList<Hotel> hotelList = new ArrayList<Hotel>();
 			ArrayList<Booking> hotelAvailable = new ArrayList<Booking>();
-			
+
 			Map<String, Integer> available = new HashMap<String, Integer>();
+			Map<String, Map<String, Integer>> listofhotels = new HashMap<String, Map<String, Integer>>();
+
+			DateFormat formatter;
+			List<Date> dates = new ArrayList<Date>();
+			List<Date> cdates = new ArrayList<Date>();
+			formatter = new SimpleDateFormat("yy-MM-dd");
 
 			// Parse Hotels.csv file
 			while ((line = br.readLine()) != null) {
@@ -42,9 +48,9 @@ public class Exercise {
 				hotelList.add(new Hotel(hotels[0], Integer.parseInt(hotels[1])));
 
 			}
-			
+
 			for (Hotel list : hotelList) {
-				System.out.println(list.getHotel()+" "+list.getRoomsAvailable());
+				System.out.println(list.getHotel() + " " + list.getRoomsAvailable());
 			}
 
 			// Parse bookings.file
@@ -55,57 +61,48 @@ public class Exercise {
 
 				// Provide list of all dates between date range
 
-				DateFormat formatter;
-				List<Date> dates = new ArrayList<Date>();
-
-				formatter = new SimpleDateFormat("yy-MM-dd");
-				Date startDate = null;
-				Date endDate = null;
-
-				startDate = (Date) formatter.parse(bookings[1]);
-				endDate = (Date) formatter.parse(bookings[2]);
-
-				long interval = 24 * 1000 * 60 * 60;
-				long startTime = startDate.getTime();
-				long endTime = endDate.getTime();
-
-				while (startTime <= endTime) {
-					dates.add(new Date(startTime));
-					startTime += interval;
-				}
-				// end date range function
+				dates = getDates(bookings[1], bookings[2]);
 
 				String bookinghotelName = bookings[0];
 				for (int i = 0; i < dates.size(); i++) {
 					Date lDate = (Date) dates.get(i);
 					String ds = formatter.format(lDate);
 					hotelAvailable.add(new Booking(bookinghotelName, ds));
-
 				}
 
 			}
 
-			System.out.println("----------");
-				for (Booking list : hotelAvailable) {
-				String listofHotelsAvailable = list.getHotelName()+"|"+list.getDates();
-					if(available.containsKey(listofHotelsAvailable))
-					{
-					available.put(listofHotelsAvailable, available.get(listofHotelsAvailable)+1);
+			for (Booking list1 : hotelAvailable) {
+				System.out.println(list1.getHotelName() + " " + list1.getDates());
+			}
+
+			System.out.println("**********");
+
+			Map<String, Integer> innerMap = new HashMap<String, Integer>();
+			for (Booking list : hotelAvailable) {
+				if (listofhotels.containsKey(list.getHotelName())) {
+
+					if (innerMap.containsKey(list.getDates())) {
+						innerMap.put(list.getDates(), innerMap.get(list.getDates()) + 1);
+						listofhotels.put(list.getHotelName(), innerMap);
+					} else {
+						innerMap.put(list.getDates(), 1);
+						listofhotels.put(list.getHotelName(), innerMap);
+
 					}
-					else
-					{
-						available.put(listofHotelsAvailable, 1);
-					}
+
+				} else {
+					innerMap.put(list.getDates(), 1);
+					listofhotels.put(list.getHotelName(), innerMap);
 				}
-				
-				
-				 for (Map.Entry<String, Integer> entry : available.entrySet()) {
-					 
-					System.out.println( entry.getKey()+" "+entry.getValue());
-				 }
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+
+			}
+
+			String checkIn = "18-05-07";
+			String checkOut = "18-05-07";
+
+			cdates = getDates(checkIn, checkOut);
+
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -121,4 +118,31 @@ public class Exercise {
 		}
 	}
 
+	public static List<Date> getDates(String sdate, String edate) {
+		DateFormat formatter;
+		List<Date> dates = new ArrayList<Date>();
+
+		formatter = new SimpleDateFormat("yy-MM-dd");
+		Date startDate = null;
+		Date endDate = null;
+
+		try {
+			startDate = (Date) formatter.parse(sdate);
+
+			endDate = (Date) formatter.parse(edate);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		long interval = 24 * 1000 * 60 * 60;
+		long startTime = startDate.getTime();
+		long endTime = endDate.getTime();
+
+		while (startTime <= endTime) {
+			dates.add(new Date(startTime));
+			startTime += interval;
+			// return dates;
+		}
+		return dates;
+	}
 }
